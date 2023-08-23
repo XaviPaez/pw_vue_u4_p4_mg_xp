c7d284dc1bd49e609cd6ec460078a4e9fc551336
Michael Garcia <msgr.2300290992@gmail.com>
2023-08-22 19:50:36 -0500

    Taller 43: Cliente Vue Api asegurada

--- src/modules/estudiante/helpers/EstudianteCliente.js
+++ src/modules/estudiante/helpers/EstudianteCliente.js
@@ -1,83 +1,62 @@
 import axios from "axios"
 
- 
-
 export const obtenerEstudianteFachada = async (cedula) => {
-
     console.log('AXIOS')
-
     return await obtenerEstudianteAPIAxios(cedula);
-
 }
-
- 
 
 export const ingresarEstudianteFachada = (bodyEstudiante) => {
-
     ingresarEstudiante(bodyEstudiante)
-
 }
-
- 
 
 export const actualizarEstudianteFachada = (bodyEstudiante, id) => {
-
     actualizarEstudiante(bodyEstudiante, id)
-
 }
-
- 
 
 export const eliminarEstudianteFachada = async (id) => {
-
- 
     return await eliminarEstudiante(id)
-
 }
-
- 
 
 const obtenerEstudianteAPI = async (cedula) => {
-
-    const data = await fetch(`http://localhost:8080/API/V1.0/Matricula/estudiantes/${cedula}`).then(r => r.json());
-
+    const data = await fetch(`http://localhost:8081/API/V1.0/Matricula/estudiantes/${cedula}`).then(r => r.json());
     console.log(data);
-
     return data;
-
 }
-
- 
 
 const obtenerEstudianteAPIAxios = async (cedula) => {
-
-    const data=axios.get(`http://localhost:8080/API/V1.0/Matricula/estudiantes/${cedula}`).then(r => r.data)
-
+    //En el config vamos a enviar todas los datos de cabecera
+    const headers = {
+        "Authorization": "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJtc2dhcmNpYXIxIiwiaWF0IjoxNjkyNzUwMjAyLCJleHAiOjE2OTI3NjAyMDJ9.K1a1VxxNOmlqM6P54VsJJwwnSyRhLz2JEPRg26bxW3AqZfxf9JcCx6ZnK3scRLw3GoYnX0UExFk6Jkjk4_FPhA",
+        "Mensaje": "Valor1"
+    }
+    const data = axios.get(`http://localhost:8081/API/V1.0/Matricula/estudiantes/${cedula}`, { headers: headers }).then(r => r.data);
     console.log(data);
-
     return data;
-
 }
 
- 
-
 const ingresarEstudiante = (bodyEstudiante) => {
-
-    axios.post(`http://localhost:8080/API/V1.0/Matricula/estudiantes`,bodyEstudiante).then(r=>r.data)
-
+    const headers = {
+        "Authorization": "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJtc2dhcmNpYXIxIiwiaWF0IjoxNjkyNzUwMjAyLCJleHAiOjE2OTI3NjAyMDJ9.K1a1VxxNOmlqM6P54VsJJwwnSyRhLz2JEPRg26bxW3AqZfxf9JcCx6ZnK3scRLw3GoYnX0UExFk6Jkjk4_FPhA",
+        "Mensaje": "Valor1"
+    }
+    axios.post(`http://localhost:8081/API/V1.0/Matricula/estudiantes`, bodyEstudiante, { headers: headers }).then(r => r.data);
+    console.log("Se inserto un usuario")
 }
 
 const actualizarEstudiante = (bodyEstudiante, id) => {
-
-
-    axios.put(`http://localhost:8080/API/V1.0/Matricula/estudiantes/${id}`, bodyEstudiante).then(r=>r.data)
-
+    const headers = {
+        "Authorization": "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJtc2dhcmNpYXIxIiwiaWF0IjoxNjkyNzUwMjAyLCJleHAiOjE2OTI3NjAyMDJ9.K1a1VxxNOmlqM6P54VsJJwwnSyRhLz2JEPRg26bxW3AqZfxf9JcCx6ZnK3scRLw3GoYnX0UExFk6Jkjk4_FPhA",
+        "Mensaje": "Valor1"
+    }
+    axios.put(`http://localhost:8081/API/V1.0/Matricula/estudiantes/${id}`, bodyEstudiante, { headers: headers }).then(r => r.data);
 }
 
 const eliminarEstudiante = async (id) => {
-
-   const data =  axios.delete(`http://localhost:8080/API/V1.0/Matricula/estudiantes/${id}`).then(r=>r.data)
-   console.log(data);
-   return data;
-
+    const headers = {
+        "Authorization": "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJtc2dhcmNpYXIxIiwiaWF0IjoxNjkyNzUwMjAyLCJleHAiOjE2OTI3NjAyMDJ9.K1a1VxxNOmlqM6P54VsJJwwnSyRhLz2JEPRg26bxW3AqZfxf9JcCx6ZnK3scRLw3GoYnX0UExFk6Jkjk4_FPhA",
+        "Mensaje": "Valor1"
+    }
+    const data = axios.delete(`http://localhost:8081/API/V1.0/Matricula/estudiantes/${id}`, { headers: headers }).then(r => r.data);
+    console.log(data);
+    return data;
 }
